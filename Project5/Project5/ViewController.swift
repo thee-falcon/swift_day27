3ea8e1318ea8c91f810461789938c636641ad06b
thee-falcon <omar.makran.10@gmail.com>
2024-03-24 22:25:45 +0000

    Working in Word Scramble

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Omar Makran on 3/24/24.
+//  Copyright © 2024 Omar Makran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
