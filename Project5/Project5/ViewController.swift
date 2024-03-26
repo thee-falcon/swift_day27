b0e0e7b1ca0d94bddc09c4c90d36a9bf97483c14
thee-falcon <omar.makran.10@gmail.com>
2024-03-26 02:33:34 +0000

    Reading from disk: contentsOf, All done

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UITableViewController {
 
         // for searching of the filw 'start.txt' in the Bundle.
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: ".txt") {
+            // Tries to read the entire content of the file at startWordsURL and creates a String from it.
             if let startWord = try? String(contentsOf: startWordsURL) {
                 // splits the string into an array of substrings using the newline.
                 allWords = startWord.components(separatedBy: "\n")
@@ -53,10 +54,22 @@ class ViewController: UITableViewController {
         useWords.count
     }
     
-    // returns a UITableViewCell.
+    // to fill each row in a UITableView with a configured cell containing the appropriate data from an array (useWords in this case).
+    /* cellForRowAt: method will configure each card (cell) with a word name from the useWords array. */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // as pre-made cards or templates designed to fit into each row of the UITableView.
+        /*
+         As you scroll down, the top-most cell scrolls off the screen.
+         Instead of destroying this cell, the system keeps it in a pool of reusable cells.
+         When a new row comes into view at the bottom, the system dequeues one of the previously used cells from the pool, reconfigures it with new data, and displays it as the new bottom row.
+         */
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        /* is responsible for setting the text of the textLabel property of the dequeued cell based on the data from the useWords array. */
         cell.textLabel?.text = useWords[indexPath.row]
+        
+        // returns the configured cell to be displayed.
         return cell
     }
 
